fa1421fb698eb22036a849ce8c480d8141d9bda4
Tolga Çobanoğlu <76070986+tolgacobanoglu@users.noreply.github.com>
2021-03-06 20:47:56 +0300

    Add files via upload

---
+++ HackerRank Solutions/Welcome The Java/src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        message();
+    }
+
+    public static void message()
+    {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
