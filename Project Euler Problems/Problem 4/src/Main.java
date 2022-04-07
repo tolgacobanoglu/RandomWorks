c12812ef1ad53750b3e3d9451790855e2ff42707
Tolga Çobanoğlu <76070986+tolgacobanoglu@users.noreply.github.com>
2022-04-07 12:27:54 +0300

    Delete Project Euler Problems directory

--- Project Euler Problems/Problem 4/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        PalindromeNumber largestPalindromeNumber3Digit=new PalindromeNumber();
-
-        largestPalindromeNumber3Digit.product();
-    }
-}
